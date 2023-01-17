78d7ebf30a09907559d3b245dfd4cf5141bd17a4
prasunmatrix <prasun@matrixnmedia.com>
2023-01-17 15:59:49 +0530

    HTTP post request

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import PostList from './components/PostList';
+import PostForm from './components/PostForm';
 
 function App() {
   return (
@@ -19,7 +20,8 @@ function App() {
           Learn React
         </a>
       </header> */}
-      <PostList />
+      <PostList /><br/>
+      <PostForm /> 
     </div>
   );
 }
